1560644165cead367a8d917b0f1a9813a2b156a8
Ricardo Azzi Silva <ricardoazzi91@hotmail.com>
2022-01-30 18:39:35 -0300

    Atualizaçao de arquivo gerado e formataçao dos cenarios de teste

--- tests/especies.test.ts
+++ tests/especies.test.ts
@@ -126,141 +126,140 @@ describe('espécies', () => {
 
   it('[espécie parcial com quebra na sindrome de dispersão]', () => {
     expect(metaDocumento[0]?.tipos[0].familias[27].especies[2]).to.eql({
-      "nome": "Abarema langsdorffii (Benth.) Barneby & J.",
-      "type": "head",
-      "nomePopular": "raposeira-branca, timbuva, olho-de-",
-      "tamanho": "3-10",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "AUT/",
-      "bioma": "Restinga/ Floresta Ombrófila Densa"
+      nome: "Abarema langsdorffii (Benth.) Barneby & J.",
+      type: "head",
+      nomePopular: "raposeira-branca, timbuva, olho-de-",
+      tamanho: "3-10",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "AUT/",
+      bioma: "Restinga/ Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie parcial]', () => {
     expect(metaDocumento[0]?.tipos[0].familias[27].especies[3]).to.eql({
-      "nome": "W. Grimes",
-      "type": "tail",
-      "nomePopular": "pomba",
-      "sindromeDispersao": "ZOO"
+      nome: "W. Grimes",
+      type: "tail",
+      nomePopular: "pomba",
+      sindromeDispersao: "ZOO"
     });
   });
 
   it('[espécie com os dois grupos funcionais]', () => {
     expect(metaDocumento[0]?.tipos[0].familias[18].especies[6]).to.eql({
-      "nome": "* Parinari brasiliensis (Schott) Hook.f.",
-      "type": "full",
-      "tamanho": "18",
-      "classeSucessional": "P/NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "ZOO",
-      "bioma": "ZOO Floresta Ombrófila Densa"
+      nome: "* Parinari brasiliensis (Schott) Hook.f.",
+      type: "full",
+      tamanho: "18",
+      classeSucessional: "P/NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "ZOO",
+      bioma: "Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie com os dois grupos funcionais]', () => {
     expect(metaDocumento[0]?.tipos[2].familias[13].especies[3]).to.eql({
-      "nome": "Leandra australis (Cham.) Cogn.",
-      "type": "full",
-      "nomePopular": "pixirica",
-      "tamanho": "0,4-3",
-      "classeSucessional": "P/NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "ZOO",
-      "bioma": "Floresta Ombrófila Densa"
+      nome: "Leandra australis (Cham.) Cogn.",
+      type: "full",
+      nomePopular: "pixirica",
+      tamanho: "0,4-3",
+      classeSucessional: "P/NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "ZOO",
+      bioma: "Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie com tamanho em decimal]', () => {
     expect(metaDocumento[0]?.tipos[2].familias[13].especies[17]).to.eql({
-      "nome": "Ossaea meridionalis D´El Rei Souza",
-      "type": "full",
-      "nomePopular": "",
-      "tamanho": "0,5-2,5",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "ZOO",
-      "bioma": "Restinga/ Floresta Ombrófila Densa"
+      nome: "Ossaea meridionalis D´El Rei Souza",
+      type: "full",
+      tamanho: "0,5-2,5",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "ZOO",
+      bioma: "Restinga/ Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie com tamanho em decimal]', () => {
     expect(metaDocumento[0]?.tipos[2].familias[13].especies[18]).to.eql({
-      "nome": "Ossaea sanguinea Cogn.",
-      "type": "full",
-      "nomePopular": "pixirica",
-      "tamanho": "0,5-2,5",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "ZOO",
-      "bioma": "Floresta Ombrófila Densa"
+      nome: "Ossaea sanguinea Cogn.",
+      type: "full",
+      nomePopular: "pixirica",
+      tamanho: "0,5-2,5",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "ZOO",
+      bioma: "Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie com dados mal formados]', () => {
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[33]).to.eql({
-      "nome": "Pseudopiptadenia warmingii (Benth.)",
-      "type": "head",
-      "nomePopular": "cauvi, cambuí-vinhático, angico-cam-",
-      "tamanho": "16-35",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "AUT",
-      "bioma": "Floresta Ombrófila Densa"
+      nome: "Pseudopiptadenia warmingii (Benth.)",
+      type: "head",
+      nomePopular: "cauvi, cambuí-vinhático, angico-cam-",
+      tamanho: "16-35",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "AUT",
+      bioma: "Floresta Ombrófila Densa"
     });
 
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[34]).to.eql({
-      "nome": "G.P.Lewis & M.P.Lima",
-      "type": "tail",
-      "nomePopular": "buim"
+      nome: "G.P.Lewis & M.P.Lima",
+      type: "tail",
+      nomePopular: "buim"
     });
   });
 
   it('[espécie número 3 no ínicio muito distante]', () => {
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[38]).to.eql({
-      "nome": "3      Senegalia polyphylla (DC.) Britton &",
-      "type": "head",
-      "nomePopular": "monjoleiro, espinho-de-maricá, monjo-",
-      "tamanho": "15-20",
-      "classeSucessional": "P",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "AUT",
-      "bioma": "Restinga, Floresta Ombrófila Densa"
+      nome: "3      Senegalia polyphylla (DC.) Britton &",
+      type: "head",
+      nomePopular: "monjoleiro, espinho-de-maricá, monjo-",
+      tamanho: "15-20",
+      classeSucessional: "P",
+      grupoFuncional: "D",
+      sindromeDispersao: "AUT",
+      bioma: "Restinga, Floresta Ombrófila Densa"
     });
 
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[38]).to.eql({
-      "nome": "Rose",
-      "type": "tail",
-      "nomePopular": "leiro-branco"
+      nome: "Rose",
+      type: "tail",
+      nomePopular: "leiro-branco"
     });
 
     expect(metaDocumento[0]?.tipos[0].familias[31].especies[25]).to.eql({
-      "nome": "*  Ocotea catharinensis Mez",
-      "type": "full",
-      "nomePopular": "canela-coqueiro",
-      "tamanho": "30",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "ZOO",
-      "bioma": "Floresta Ombrófila Densa"
+      nome: "*  Ocotea catharinensis Mez",
+      type: "full",
+      nomePopular: "canela-coqueiro",
+      tamanho: "30",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "ZOO",
+      bioma: "Floresta Ombrófila Densa"
     });
   });
 
   it('[espécie com quebra de linha na sindrome de dispersão]', () => {
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[29]).to.eql({
-      "nome": "Ormosia arborea (Vell.) Harms",
-      "type": "head",
-      "nomePopular": "olho-de-cabra, olho-de-cabra-vermelho",
-      "tamanho": "15-20",
-      "classeSucessional": "NP",
-      "grupoFuncional": "D",
-      "sindromeDispersao": "AUT/",
-      "bioma": "Restinga, Floresta Ombrófila Densa"
+      nome: "Ormosia arborea (Vell.) Harms",
+      type: "head",
+      nomePopular: "olho-de-cabra, olho-de-cabra-vermelho",
+      tamanho: "15-20",
+      classeSucessional: "NP",
+      grupoFuncional: "D",
+      sindromeDispersao: "AUT/",
+      bioma: "Restinga, Floresta Ombrófila Densa"
     });
 
     expect(metaDocumento[1]?.tipos[0].familias[25].especies[30]).to.eql({
-      "type": "tail",
-      "sindromeDispersao": "ZOOM"
+      type: "tail",
+      sindromeDispersao: "ZOOM"
     });
   });
 
